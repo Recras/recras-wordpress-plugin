dc6fbb057e90b471204492efe76dadb79c7a61a4
Sander de Jong <sander@recras.nl>
2022-08-30 11:09:37 +0200

    Book process discount styling improvement

--- src/Plugin.php
+++ src/Plugin.php
@@ -282,7 +282,7 @@ class Plugin
         if ($theme) {
             $allowedThemes = Settings::getThemes();
             if ($theme !== 'none' && array_key_exists($theme, $allowedThemes)) {
-                wp_enqueue_style('recras_theme_base', $this->baseUrl . '/css/themes/base.css', [], '5.1.0');
+                wp_enqueue_style('recras_theme_base', $this->baseUrl . '/css/themes/base.css', [], '5.1.3');
                 wp_enqueue_style('theme_' . $theme, $this->baseUrl . '/css/themes/' . $theme . '.css', [], $allowedThemes[$theme]['version']);
             }
         }
