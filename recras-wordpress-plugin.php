8d00e138f3a9f6dec19d3a2fdcad8e305c6500ce
Sander <sander@recras.nl>
2025-04-04 10:06:36 +0200

    Plugin now requires WP 6.6 or higher

--- recras-wordpress-plugin.php
+++ recras-wordpress-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Recras WordPress Plugin
 Plugin URI: https://www.recras.nl/
 Version: 6.3.3
 Description: Easily integrate your Recras data into your own site
-Requires at least: 6.5
+Requires at least: 6.6
 Requires PHP: 7.4.0
 Author: Recras
 Author URI: https://www.recras.nl/
